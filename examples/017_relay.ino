0226f286262c54131d1e6c75aa351a93c54871a9
sotojang <mrjang28@gmail.com>
2020-10-07 22:53:14 +0900

    Create 017_relay.ino

---
+++ examples/017_relay.ino
@@ -0,0 +1,14 @@
+#define Relay  2                            //적외선 리모턴 1번 버튼에 제어될 LED를 디지털 포트 2번에 연결시켜주세요.
+ 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Relay , OUTPUT);                 //디지털 포트 2번을 OUT으로 설정
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(Relay , HIGH);               // COM(공통단자)와 NC(Nomal Close)단자 접점형성
+  delay(1000);
+  digitalWrite(Relay , LOW);              // COM(공통단자)와 NO(Nomal OPEN)단자 접점형성
+  delay(1000); 
+}
