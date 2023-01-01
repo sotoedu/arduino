0a0db7f9e8664c9e4100d1ea15cebe44bbb17ba8
sotojang <mrjang28@gmail.com>
2023-01-01 16:22:36 +0900

    Update 003_button.ino

--- examples/003_button.ino
+++ examples/003_button.ino
@@ -1,5 +1,13 @@
-const int LED=13;      //The LED is connected to pin 13
-const int BUTTON=8;      //The Button is connected to pin 8
+/*
+Exploring Arduino - Code Listing 3: Button-LED
+Copyright 2020 SotoLAB ( https://github.com/sotoedu/arduino )
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License v3 as published by
+the Free Software Foundation.
+*/
+
+const int LED = 13;      //The LED is connected to pin 13
+const int BUTTON = 8;      //The Button is connected to pin 8
 
 void setup()
 {
