43f20613edb729f9a62c2daa6f77d67e4d02d60a
sotojang <mrjang28@gmail.com>
2023-01-01 16:07:09 +0900

    Update 005_randomgrb.ino

--- examples/005_randomgrb.ino
+++ examples/005_randomgrb.ino
@@ -20,6 +20,15 @@ void setup() {
 
 }
 
+void RGB_color(int r_value, int g_value, int b_value)
+{// RGB_color라는 함수를 만들어서 3가지 숫자를 조합하게 만드는 함수를 제작합니다.
+ // 이 함수가 없다면 예를들어 빨간색을 추출하고 싶다면 analogWrite를 3번하여야합니다.
+ // 하지만 이 함수를 제작하여 바로 숫자만 바꾸어 실행 할 수 있습니다!
+  analogWrite(RLED, r_value);
+  analogWrite(GLED, g_value);
+  analogWrite(BLED, b_value);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   analogWrite(RLED, random(256));
@@ -28,4 +37,6 @@ void loop() {
   delay(1000);  
   Serial.print("random: ");  
   Serial.println(random(256));
+  RGB_color(0, 0, 255); // Blue
+  delay(1000);  
 }
