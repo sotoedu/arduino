59bcbe59d2ed096b58fa904a48655251e0cab0a4
sotojang <mrjang28@gmail.com>
2023-01-01 21:57:00 +0900

    Update 017_relay.ino

--- examples/017_relay.ino
+++ examples/017_relay.ino
@@ -1,3 +1,11 @@
+/*
+Exploring Arduino - Code Listing 17: RELAY
+Copyright 2020 SotoLAB ( https://github.com/sotoedu/arduino )
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License v3 as published by
+the Free Software Foundation.
+*/
+
 #define Relay  2                            //적외선 리모턴 1번 버튼에 제어될 LED를 디지털 포트 2번에 연결시켜주세요.
  
 void setup() {
