9bded538b36b014d244ca6fc03ed8c1ec242f0a4
sotojang <mrjang28@gmail.com>
2023-01-01 19:19:02 +0900

    Update 013_distLCD.ino

--- examples/013_distLCD.ino
+++ examples/013_distLCD.ino
@@ -1,8 +1,9 @@
-/* 
-위의 코드는 bitelab 블로그의 원작자가 만든 코드입니다. 
-필요하신 부분이 있으면 사용하시면 됩니다.
-사용하기 전에 댓글을 남겨 주시면 감사하겠습니다. 
-https://blog.naver.com/bitelab
+/*
+Exploring Arduino - Code Listing 13: LCD Distance PCF8574 기반 (39 = 0x27)
+Copyright 2020 SotoLAB ( https://github.com/sotoedu/arduino )
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License v3 as published by
+the Free Software Foundation.
 */
 
 #include <Wire.h>                     // i2C 통신을 위한 라이브러리
@@ -10,16 +11,16 @@ https://blog.naver.com/bitelab
 LiquidCrystal_I2C lcd(0x27,16,2);     //  0x3F or 0x27를 선택하여 주세요. 작동이 되지 않는 경우 0x27로 바꾸어주세요. 확인결과 0x3f가 작동하지 않을 수 있습니다.
 //고유주소가 LCD마다 다르기 때문입니다.
 
-#define trigPin1 8  // 초음파 발신핀 define은 쉽게말하면, 숫자의 이름을 정의해주는것. 
-#define echoPin1 7  // 초음파 수신핀
+#define echoPin 9  // 초음파 수신핀
+#define trigPin 10  // 초음파 발신핀 define은 쉽게말하면, 숫자의 이름을 정의해주는것. 
 
 //변수를 설정합니다. 
-long duration1, distance1;
+long duration, distance;
 
 void setup() 
 {
-  pinMode(trigPin1,OUTPUT);   // trig를 출력 모드로 설정
-  pinMode(echoPin1,INPUT);    // echo를 입력모드로 설정
+  pinMode(trigPin,OUTPUT);   // trig를 출력 모드로 설정
+  pinMode(echoPin,INPUT);    // echo를 입력모드로 설정
 
   Serial.begin(9600);        //시리얼 프린트 시작
   lcd.init();                // LCD 초기화
@@ -30,21 +31,20 @@ void setup()
 void loop() 
 {
   //초음파 센서를 한번 초기화 하는 과정입니다. 마치 껏다 켯다를 하면서 거리를 초기화합니다.
-  digitalWrite(trigPin1, LOW);
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin1, HIGH);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
-  duration1 = pulseIn(echoPin1, HIGH);
-  distance1= duration1*0.034/2;
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
 
   lcd.clear();
   lcd.setCursor(1,0);
   lcd.print("Distance Check");
   lcd.setCursor(4,1);
-  lcd.print(distance1); lcd.print(" cm");
+  lcd.print(distance); lcd.print(" cm");
   delay(1000);
   lcd.setCursor(1,3);
   lcd.print("Warning from crash!");
-
 }
